a0fef62728e225e9514b56e34c79fa08d14e66b3
agarwalsujal <sujalagarwa26@gmai.com>
2025-08-12 07:14:34 +0530

    refactor: Remove React Query Devtools from App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./lib/queryClient";
 import { AuthProvider } from "./contexts/AuthContext";
 import Header from "./components/layout/Header";
@@ -70,7 +69,6 @@ function App() {
           </div>
         </Router>
       </AuthProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
